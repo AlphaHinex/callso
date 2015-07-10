9c37b5974abac1f51d18e2c5df633db5cdaa3c93
AlphaHinex <AlphaHinex@gmail.com>
2015-07-10 08:30:18 +0800

    Clean jni resources

--- jni/LPR_java.cpp
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-#include "com_lpr_LPR.h"
-
-jbyteArray JNICALL Java_com_lpr_LPR_DetectLPR(JNIEnv * jnienv, jobject jobj, jshortArray intArray, jint width, jint height,jint maxSize)
-{
-    jbyteArray  returnLPRArray = jnienv->NewByteArray( 1024 );
-    return   returnLPRArray ;
-}
